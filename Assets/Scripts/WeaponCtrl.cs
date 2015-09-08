505427eb4a760357def9855aaf8807483c61baba
unknown <mc3683@columbia.edu>
2015-09-08 10:46:45 -0400

    adding support for heavy bullet

--- Assets/Scripts/WeaponCtrl.cs
+++ Assets/Scripts/WeaponCtrl.cs
@@ -17,11 +17,11 @@ public class WeaponCtrl : MonoBehaviour {
 			GameObject bullet = (GameObject)GameObject.Instantiate(bulletPrefab);
 			bullet.transform.position = transform.position;
 			bullet.transform.rotation = transform.rotation;
-			bullet.transform.position += bullet.transform.forward * 12.0f;
+			bullet.transform.position += bullet.transform.forward * 14.0f;
 		}
 		if(Input.GetMouseButtonDown(1))
 			Debug.Log("WeaponCtl - Pressed right click.");
 		if(Input.GetMouseButtonDown(2))
-			Debug.Log("WeaponCtl - Pressed middle click.");	
+			Debug.Log("WeaponCtl - Pressed middle click.");
 	}
 }
